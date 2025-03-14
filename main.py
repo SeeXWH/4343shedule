9e44f1dbe9357f9d41397f9127eaebac578d27f4
andreymikhailov.vma@gmail.com <plarroar03050@gmail.com>
2025-03-14 16:26:32 +0300

    test

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ def get_next_day(current_day: str) -> str:
 async def send_current_scheduled_messages():
     while True:
         now = datetime.now(moscow_tz).time()
-        target_time = time(16, 26)
+        target_time = time(6, 0)
 
         if now.hour == target_time.hour and now.minute == target_time.minute:
             user_ids = load_ids("users.txt")
